782d435033b8fbaa1034c99a2d4735edc9bc89eb
Abdirahman0022 <Abdirahman3878@gmail.com>
2021-12-03 22:58:47 +0300

    read

--- gitPush.sh
+++ gitPush.sh
@@ -39,12 +39,12 @@ if [ "$git_remote" = "" ]; then # git remote not defined
         echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
         git remote add origin https://github.com/${git_user_id}/${git_repo_id}.git
     else
-        git remote add origin https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git
+        git push origin https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git
     fi
 
 fi
 
-git pull origin master
+git push origin master
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://github.com/${git_user_id}/${git_repo_id}.git"
