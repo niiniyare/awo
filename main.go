d6f57c019e8687cf408c8699dbb026ae97a73804
niiniyare <abdirahman3878@gmail.com>
2022-06-30 04:45:41 +0300

    .

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"fmt"
 	"time"
+
 	"github.com/niiniyare/awo/util"
 )
 
 func main() {
-  now:=time.Now()
+	now := time.Now()
 	for a := 1; a < 10000; a++ {
 		//	RandomOwner()
-
 		fmt.Printf("%d:  %v \n", a, util.RandomString(6))
 
 	}
-diff:=now.Sub(time.Now())
+	diff := now.Sub(time.Now())
 
-fmt.Println("Elapsed MiliSeconds: ",diff.Milliseconds())
+	fmt.Println("Elapsed MiliSeconds: ", diff.Milliseconds())
 }
