f593869d6d0055fd26a98c931ae60bf98518caf4
Abdirahman ahmed <abdirahman3878@gmail.com>
2022-09-18 22:21:07 +0300

    alot happened 😆

---
+++ script/test_coverage_browser.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+
+rm -rf ~/storage/shared/Download/coverage.html
+
+go test -v -cover -coverprofile=coverage.out ./db/sqlc/...
+
+go tool cover -html=coverage.out
+
+lsd -t /data/data/com.termux/files/usr/tmp/ > ./script/coverage.txt
+
+file=/data/data/com.termux/files/usr/tmp/$(head -n+1 ./script/coverage.txt)/coverage.html
+
+
+termux-open $file
+# $HOME/awo/db/sqlc
