0f52d5d125271c7fdba8d394e7c1dd28efce6d66
Abdiraman Ahmed <Abdiraman3878@gmail.com>
2023-09-29 22:21:05 +0300

    sqlc actions added

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -9,9 +9,9 @@ import (
 // Store defines all functions to execute db queries and transactions
 type Store interface {
 	Querier
-	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
-	CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error)
-	VerifyEmailTx(ctx context.Context, arg VerifyEmailTxParams) (VerifyEmailTxResult, error)
+	// TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+	// CreateUserTx(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error)
+	// VerifyEmailTx(ctx context.Context, arg VerifyEmailTxParams) (VerifyEmailTxResult, error)
 }
 
 // SQLStore provides all functions to execute SQL queries and transactions
